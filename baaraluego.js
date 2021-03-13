10ae7ede1f62c9887ca7e526a1dea96cfa00d75a
asmedina24 <alejandramedina1215@gmail.com>
2021-03-12 22:29:10 -0300

    second commit

---
+++ baaraluego.js
@@ -0,0 +1,15 @@
+
+
+    // ALEJANDRA MEDINA´s cycle 
+    let me = "code lover"
+    while (me.isAlive()){
+        me.code();
+        me.eat();
+        me.giveThanksToGod();
+        me.sleep();  
+    }
+
+
+
+
+
